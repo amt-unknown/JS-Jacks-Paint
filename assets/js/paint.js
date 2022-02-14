13c451dccc44ff35e39e7bd6b713a61d2bc1300c
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-02-14 14:52:59 -0800

    Project Complete

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,98 +1,94 @@
-function configureListeners() {
-    var images = document.getElementsByTagName('img');    
+function configureListeners(){
+    var images = document.getElementsByTagName('img')
 
-     for (var i = 0; i < images.length; i++) {        
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
-    } 
+    for(let i=0; i < images.length; i++){
+    
+        images[i].addEventListener('mouseover', addOpacity, false)
+        images[i].addEventListener('mouseout', removeOpacity, false)
+    }
 }
 
-function addOpacity(event) {
-    if (!this.classList.contains('dim')){
-        this.classList.add('dim')
-    }    
-    getProductInfo(event.target.id);     
+function addOpacity(event){
+    if(!this.classList.contains('dim')){
+        this.classList.add("dim");
+    }
+    getProductInfo(this.id)
 }
 
-function removeOpacity(event) {
-     if (this.classList.contains('dim')){
+function removeOpacity(event){
+    if(this.classList.contains('dim')){
         this.classList.remove('dim');
     }
 
-    let element = document.getElementById('ppg');
-        element.textContent = '';
-        
-    let color = document.getElementById('color');
-        color.textContent = ''; 
-
-    event.preventDefault();    
+    document.getElementById('color').textContent = "";
+    document.getElementById('ppg').textContent = "";
 }
 
-function changeImage(elementId) {
-    let image = document.getElementById('imgDisplay');
-    image.src = elementId.src;
-}
 
-function getProductInfo(partNumber) {
+function getProductInfo(productID){
+    let colorName;
     let price;
-    let colorName;  
-    
-    switch (partNumber) {
-        case 'pn1':           
-            price = '$19.99'
-            colorName = 'Lime Green'
-            updatePrice(colorName, price)           
-            break;           
+
+    switch (productID){
+        case 'pn1':
+            colorName = "Lime Green";
+            price = "$14.99";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn2':
-            price = '$12.99'
-            colorName = 'Medium Brown' 
-            updatePrice(colorName, price)     
-            break;            
+            colorName = "Medium Brown";
+            price = "$11.14";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn3':
-            price = '$11.99'
-            colorName = 'Royal Blue'   
-            updatePrice(colorName, price)   
-            break;   
+            colorName = "Royal Blue";
+            price = "$22.99";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn4':
-            price = '$14.99'
-            colorName = 'Bright Red'            
-            updatePrice(colorName, price)   
-            break;   
+            colorName = "Solid Red";
+            price = "$13.42";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn5':
-            price = '$9.99'
-            colorName = 'Solid White' 
-            updatePrice(colorName, price)              
-            break;   
+            colorName = "Solid White";
+            price = "$21.98";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn6':
-            price = '$15.99'
-            colorName = 'Solid Black'   
-            updatePrice(colorName, price)            
-            break;   
+            colorName = "Solid Black";
+            price = "$4.99";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn7':
-            price = '$8.99'
-            colorName = 'Medium Blue'       
-            updatePrice(colorName, price)        
-            break;   
+            colorName = "Solid Cyan";
+            price = "$8.22";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn8':
-            price = '$16.99'
-            colorName = 'Light Purple'      
-            updatePrice(colorName, price)         
-            break;   
+            colorName = "Solid Purple";
+            price = "$11.99";
+            updateInfo(colorName, price)
+        break;
+        
         case 'pn9':
-            price = '$17.99'
-            colorName = 'Bright Yellow'                       
-            updatePrice(colorName, price)   
-            break;   
-          default:              
+            colorName = "Solid Yellow";
+            price = "$14.99";
+            updateInfo(colorName, price)
+        break;
+
+
     }
 
-    function updatePrice(colorName, price)
-    {       
-        let ppg = document.getElementById('ppg');
-        ppg.textContent = price;
-        
-        let color = document.getElementById('color');
-        color.textContent = colorName;
+    function updateInfo(colorName, price){
+        document.getElementById('color').textContent = colorName;
+        document.getElementById('ppg').textContent = price;
     }
-    
 }
